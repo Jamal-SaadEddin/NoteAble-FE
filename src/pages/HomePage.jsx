5719d8eea4a95e343db2dfc223743fa09426ce07
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-06-03 20:41:17 +0300

    Add note cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import Header from "../components/Header";
 import { Box } from "@chakra-ui/react";
+import React from "react";
+import Notes from "../components/Notes";
+import Header from "../components/Header";
 
 const HomePage = () => {
   return (
-    <Box p={5}>
+    <Box p={5} display="flex" gap={7} flexDir="column">
       <Header />
+      <Notes />
     </Box>
   );
 };
